9d6701f3b3c125e26750c42f984bdacdeb2bb0f9
Ahmed Mohammed Yamany <yamany2862016@gmail.com>
2024-05-23 13:25:40 +0300

    Image+uiImage

--- Sources/UIExtensions/Image/UIImage+to.swift
+++ Sources/UIExtensions/Image/UIImage+to.swift
@@ -5,7 +5,7 @@
 //  Created by Ahmed Yamany on 22/04/2024.
 //
 
-import UIKit
+import SwiftUI
 
  public extension Array where Element == UIImage {
     func pngData() -> [Data] {
@@ -13,9 +13,9 @@ import UIKit
         map { $0.pngData() }.filter { $0 != nil }.map { $0! }
         // swiftlint: enable force_unwrapping
     }
-    func jpgData() -> [Data] {
+    func jpgData(compressionQuality: CGFloat) -> [Data] {
         // swiftlint: disable force_unwrapping
-        map { $0.jpegData(compressionQuality: 0.2) }.filter { $0 != nil }.map { $0! }
+        map { $0.jpegData(compressionQuality: compressionQuality) }.filter { $0 != nil }.map { $0! }
         // swiftlint: enable force_unwrapping
     }
 }
@@ -25,5 +25,13 @@ public extension Array where Element == Data {
         // swiftlint: disable force_unwrapping
         map { UIImage(data: $0) }.filter { $0 != nil }.map { $0! }
         // swiftlint: enable force_unwrapping
+    }
+}
+
+@available(iOS 16.0, *)
+public extension Image {
+    @MainActor
+    func uiImage() -> UIImage? {
+        return ImageRenderer(content: self).uiImage
     }
 }
